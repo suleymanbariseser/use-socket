f4697275671c80da977615169d53cb02bddd30f4
Suleyman Baris Eser <suleymanbariseser@gmail.com>
2023-03-15 21:08:54 +0300

    add prettier config

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,28 @@
-const peerDepsExternal = require("rollup-plugin-peer-deps-external");
-const resolve = require("@rollup/plugin-node-resolve");
-const commonjs = require("@rollup/plugin-commonjs");
-const typescript = require("rollup-plugin-typescript2");
+const peerDepsExternal = require('rollup-plugin-peer-deps-external');
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('rollup-plugin-typescript2');
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
-module.exports = {
-  input: "src/index.ts",
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-  ],
+module.exports =  {
+	input: 'src/index.ts',
+	output: [
+		{
+			file: packageJson.main,
+			format: 'cjs',
+			sourcemap: true,
+		},
+		{
+			file: packageJson.module,
+			format: 'esm',
+			sourcemap: true,
+		},
+	],
+	plugins: [
+		peerDepsExternal(),
+		resolve(),
+		commonjs(),
+		typescript({ useTsconfigDeclarationDir: true }),
+	],
 };
